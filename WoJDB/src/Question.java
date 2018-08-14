5fb3fc29aec436cb796a12aacdd8a320f5a71297
MisterMiracle <jakemanr12@gmail.com>
2018-08-14 19:29:25 -0400

    Added WoJDB
    Added Java database connection (or at least the start of it)

---
+++ WoJDB/src/Question.java
@@ -0,0 +1,12 @@
+
+public class Question {
+
+	
+	
+	public static void main(String[] args) {
+		
+	}
+	
+	
+
+}
---
+++ WoJDB/src/WoJDB.java
@@ -0,0 +1,49 @@
+import java.sql.*;
+import javax.sql.*;
+
+public class WoJDB {
+
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void connectDB() {
+		
+	}
+	
+	private void disconnectDB() {
+		
+	}
+	
+	public void get6Categories () {
+		
+		
+		
+	}
+	
+	public void getAllCategories() {
+		
+		
+	}
+	
+	public void getCategory (String catID) {
+		
+	}
+	
+	public void addCategory (String catName, Question[] questions){
+		
+	}
+	
+	public void editCategory (String catID, String catName, Question[] questions) {
+		
+	}
+	
+	public void deleteCategory (String catID) {
+		
+	}
+	
+}
